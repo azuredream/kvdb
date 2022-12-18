23625b762ad55444c460828cfa658fb341c89406
George <zhaozixuan67@gmail.com>
2022-12-17 23:40:54 -0500

    newnode newwskiplist

--- utils/skiplist.go
+++ utils/skiplist.go
@@ -63,29 +63,30 @@ type node struct {
 
 type skiplist struct {
 	arena *Arena,
-	head uint32,
+	headoffset uint32,
 	h uint32,
 	ref uint32, //?
 }
 
-type valuenode struct {
 
-}
-// new node
-// node
-// 	setvalue(value)
-// 	getvalue()
-// 	getkey()
-// 	getnext(level) //atomic load
-// 	setnext(oldoffset, newoffset, level)//atomic cas
-
+// type skiplist struct {
+// 	arena *Arena,
+// 	headoffset uint32,
+// 	h uint32,
+// 	ref uint32, //?
+// }
 func newskiplist (arenasize uint64) *skiplist {
 	arena := newArena(arenasize)
 	//get head
 	nodesize := MAXNODE
 	head  := newnode(arena, nil, nil, MAXHEIGHT)
-
-
+	headoffset := uint32(uintprt(unsafe.Pointer(head)) - uintprt(unsafe.Pointer(arena.buf[0])))
+	return &skiplist{
+		arena: arena,
+		headoffset: headoffset,
+		h: 1,
+		ref: 0,
+	}
 
 }
 
@@ -97,27 +98,29 @@ func newskiplist (arenasize uint64) *skiplist {
 // 	tower [MAXHEIGHT]uint32,
 // }
 
-func newnode (a arena, k []byte, v []byte, height uint32) nodeoffset uint32 {
+func newnode (a arena, k []byte, v []byte, height uint32) *node {
 	//set up node meta data
 	nodeoffset = a.allocate(MAXNODE);
 	keysize := uint32(len(k))
 	koffset := a.allocate(keysize)
 	vsize := uint32(len(v))
 	voffset := a.allocate(vsize)
-
-	//setup k
 	node := (*node)unsafe.Pointer(&s.buf[koffset])
-	
-
+	node.keysize = keysize
+	node.koffset = koffset
+	node.v = encodev(vsize, voffset)
+	node.height = height
+	//setupk
+	AssertTrue(len(k) == copy(a.buf[koffset:koffset+ksize], k))
 	//setup v
-
+	AssertTrue(len(v) == copy(a.buf[voffset:voffset+vsize], v))
 	//
-
+	return node
 
 }
 
 func (n* node) encodev(vsize uint32, voffset uint32) uint64 {
-	return (uint64(vsize<<32) & uint64(voffset)
+	return uint64(vsize<<32) | uint64(voffset)
 }
 
 func (n* node) decodev(codedv uint64) vsize uint32, voffset uint32 {
